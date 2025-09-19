338381915c0d9422ce99f70b75a8dc5bbe4381c5
YangYoungHwan00 <qjddldi17@gmail.com>
2025-09-19 15:15:15 +0900

    add nodemon setting

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hey");
+    res.send("Wowgood");
 });
 
 app.listen(PORT, () => {
-    console.log('Server running at http:localhost:${PORT}');
+    console.log(`Server running at http:localhost:${PORT}`);
 });
