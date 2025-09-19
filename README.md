8c4a880fb02331cc210fb28964ba0215fd85ce69
YangYoungHwan00 <qjddldi17@gmail.com>
2025-09-19 18:01:03 +0900

    09/19作業

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Cal_kenshuu
 キャル研修発表作品
+
+# Client
+# 実行
+npm run dev
+
+
+###Server
+
+# 開発モード実行（自動サーバー再起動）
+npm run dev
+
+# 一般実行
+npm start
+node server.js
--- server/server.js
+++ server/server.js
@@ -1,10 +1,35 @@
 const express = require("express");
+const cors = require("cors");
+// //pos1
+// const { Pool } = require("pg");
+
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Wowgood");
+app.use(cors());
+app.use(express.json());
+// //pos2
+// const pool = new Pool({
+//     user: "testuser",
+//     host: "localhost",
+//     database: "postgres",
+//     password: "testpass",
+//     port: 5432
+// });
+
+app.get("/api/hello", (req, res) => {
+    res.json({ message: "yes" });
 });
+// //pos3
+// app.get("/api/users", async (req, res) => {
+//     try {
+//         const result = await pool.query("SELECT * FROM users");
+//         res.json(result.rows);
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).send("DB error");
+//     }
+// });
 
 app.listen(PORT, () => {
     console.log(`Server running at http:localhost:${PORT}`);
