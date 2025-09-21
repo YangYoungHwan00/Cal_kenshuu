671a059b7f4a7caad7e190783243702912f62cc0
양영환 <yyh@yang-yeonghwan-ui-MacBookAir.local>
2025-09-21 10:40:45 +0900

    connect db

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   const [msg, setMsg] = useState("")
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/hello")
+    fetch("http://localhost:3000")
       .then((res) => res.json())
       .then((data) => setMsg(data.message));
   }, []);
