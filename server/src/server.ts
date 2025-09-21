caee176be2d1e031c49619a2db4f0e1cdf0ec111
https://github.com/YangYoungHwan00 <qjddldi17@gmail.com>
2025-09-21 17:34:34 +0900

    move server.ts into src folder

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,56 +1,6 @@
-// require("dotenv").config();
-
-// const express = require("express");
-// const cors = require("cors");
-// const bcrypt = require("bcrypt");
-// const { Pool } = require("pg");
-
-// const app = express();
-// const PORT = 3000;
-// const hashedPassword = await bcrypt.hash(plainPassword, 10);
-
-
-// app.use(cors());
-// app.use(express.json());
-
-// //환경 변수를 사용해 데이터베이스 연결 풀 생성
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-// });
-
-// //데이터베이스 연결 테스트 예시
-// app.get('/test-db', async (req, res) => {
-//     try {
-//         const client = await pool.connect();
-//         const result = await client.query('SELECT NOW()');
-//         client.release();
-//         res.send(`DB Connection Success: ${result.rows[0].now}`);
-//     } catch (err) {
-//         console.error('Database connection error', err);
-//         res.status(500).send('DB Connection Error');
-//     }  
-// }); 
-
-// app.get("/", (req, res) => {
-//     res.json({ message: "yes" });
-// });
-
-// app.get("/api/users", async (req, res) => {
-//     try {
-//         const result = await pool.query("SELECT * FROM users");
-//         res.json(result.rows);
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).send("DB error");
-//     }
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`Server running at http:localhost:${PORT}`);
-// });
-
 import 'dotenv/config';
 import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 import cors from 'cors';
 import bcrypt from 'bcrypt';
 import { Pool, QueryResult } from 'pg';
@@ -58,7 +8,12 @@ import { Pool, QueryResult } from 'pg';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(cors());
+// 유효성 검사 미들웨어
+const validateRegistration = [
+    body('username').isLength({ min: 3}).withMessage('ユーザネームは２文字以上必要です'),
+    body('password').isLength({ min: 6}).withMessage('パスワードは６文字以上必要です')
+];
+
 app.use(express.json());
 app.use(cors());
 
@@ -99,6 +54,38 @@ app.get('/api/users', async (req: Request, res: Response) => {
     } catch (err) {
         console.error('DB error:', err);
         res.status(500).send('DB error');
+    }
+});
+
+//회원가입
+app.post('/api/register', validateRegistration, async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    
+    const { username, password } = req.body;
+
+    try {
+        const existingUser = await pool.query('SELECT * FROM users WHERE username = $1', [username])
+        if (existingUser.rows.length > 0) {
+            return res.status(409).json({ message: 'ユーザネームは既に存在しています' });
+        }
+
+        //비밀번호 해싱
+        const saltRounds = 10;
+        const passwordHash = await bcrypt.hash(password, saltRounds);
+
+        //db에 사용자 정보 저장
+        await pool.query(
+            'INSERT INTO users (username, password_hash) VALUES ($1, $2)',
+            [username, passwordHash]
+        );
+
+        res.status(201).json({ message: 'ユーザ登録成功' });
+    } catch (err) {
+        console.error('Registration error:', err);
+        res.status(500).send('Registration error');
     }
 });
 
